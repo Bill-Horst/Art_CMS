ec478c05395387c1e05c2abad94afab9dec4e91f
Bill Horst <bill@billhorst.com>
2018-07-15 20:48:51 -0600

    adjusted storage so only prod environment uses AWS

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,7 +9,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
-  storage :aws
+  # storage :aws
+
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.production?
+    storage :aws
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
